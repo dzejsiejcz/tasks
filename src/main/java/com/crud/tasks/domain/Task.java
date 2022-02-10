ee8b33eb5514f2e560ace889556fa67d64895276
dzejsiejcz <joel.chawraj@gmail.com>
2022-02-10 23:51:35 +0100

    first

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -1,5 +1,10 @@
 package com.crud.tasks.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class Task {
     private Long id;
     private String title;
--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -1,5 +1,11 @@
 package com.crud.tasks.domain;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class TaskDto {
     private Long id;
     private String title;
