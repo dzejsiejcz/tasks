8bc850f8ec201dd3a4d842d7a3cc734ed28d89b0
dzejsiejcz <joel.chawraj@gmail.com>
2022-02-16 23:19:11 +0100

    19.2

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -5,11 +5,10 @@ import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/v1/tasks")
@@ -27,8 +26,9 @@ public class TaskController {
     }
 
     @GetMapping(value = "{taskId}")
-    public TaskDto getTask(@PathVariable Long taskId) {
-        return new TaskDto(1L, "test title", "test_content");
+    public Task getTask(@PathVariable Long taskId) {
+        Optional<Task> optionalTask = service.getTask(taskId);
+        return optionalTask.map(taskMapper::mapToTask).orElse(null);
     }
 
     @DeleteMapping
