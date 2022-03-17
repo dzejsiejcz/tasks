d858a073ca4682a5e86c53123f5e31c7ff826fff
Joel Chawraj <joel.chawraj@gmail.com>
2022-03-17 22:13:10 +0100

    22.3

---
+++ src/main/java/com/crud/tasks/domain/Trello.java
@@ -0,0 +1,16 @@
+package com.crud.tasks.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Trello {
+
+    @JsonProperty("board")
+    private int board;
+
+    @JsonProperty("card")
+    private int card;
+}
