e3dcb32b8a2de26d69707e7d7f14d4fb33dafa3d
Joel Chawraj <joel.chawraj@gmail.com>
2022-03-21 00:03:30 +0100

    23.2

---
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -0,0 +1,13 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Mail {
+
+    private final String mailTo;
+    private final String subject;
+    private final String message;
+}
---
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -0,0 +1,36 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Mail;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class SimpleEmailService {
+
+    private final JavaMailSender javaMailSender;
+
+    public void send(final Mail mail) {
+        log.info("Starting email preparation...");
+        try {
+            SimpleMailMessage mailMessage = createMailMessage(mail);
+            javaMailSender.send(mailMessage);
+            log.info("Email has been sent.");
+        } catch (MailException e) {
+            log.error("Failed to process email sending: " + e.getMessage(), e);
+        }
+    }
+
+    private SimpleMailMessage createMailMessage(final Mail mail) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessage());
+        return mailMessage;
+    }
+}
