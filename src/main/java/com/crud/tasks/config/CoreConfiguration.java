1bc4dcbbe8568b8e670a271d123e45c53696f340
Joel Chawraj <67972266+dzejsiejcz@users.noreply.github.com>
2022-05-08 21:28:35 +0200

    29.4

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -2,19 +2,19 @@ package com.crud.tasks.config;
 
 
 import com.crud.tasks.controller.TaskController;
-import io.swagger.annotations.SwaggerDefinition;
+//import io.swagger.annotations.SwaggerDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableScheduling
-@EnableSwagger2
+//@EnableSwagger2
 @Configuration
 public class CoreConfiguration {
 
@@ -23,13 +23,13 @@ public class CoreConfiguration {
         return new RestTemplate();
     }
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.crud.tasks.controller"))
-                .paths(PathSelectors.any())
-                .build();
-    }
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.crud.tasks.controller"))
+//                .paths(PathSelectors.any())
+//                .build();
+//    }
 
 }
